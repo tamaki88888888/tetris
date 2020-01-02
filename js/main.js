e78906cd804609f0272adeb267d40962a81fc520
KoTamaki <koukoukokoko@icloud.com>
2020-01-02 18:35:36 +0900

    display tetrimino on browser

--- js/main.js
+++ js/main.js
@@ -8,8 +8,9 @@ var block_W = field_W / cols,
     block_H = field_H / rows;
 var canvas = document.getElementById('field');
 var ctx = canvas.getContext('2d');
-var current_x = 0,
+var current_x = 3,
     current_y = 0;
+var current_mino = newMino();
 ctx.strokeStyle = "black";
 ctx.strokeRect(0, 0, block_W, block_H);
 
@@ -17,8 +18,12 @@ var render = function render() {
   ctx.clearRect(0, 0, field_W, field_H);
   ctx.strokeStyle = "black";
 
-  for (var i = 0; i < cols; i++) {
-    ctx.strokeRect(i * block_W, current_y * block_H, block_W - 1, block_H - 1);
+  for (var y = 0; y < 2; y++) {
+    for (var x = 0; x < 4; x++) {
+      if (current_mino[y][x]) {
+        ctx.strokeRect((current_x + x) * block_W, (current_y + y) * block_H, block_W, block_H);
+      }
+    }
   }
 };
 
