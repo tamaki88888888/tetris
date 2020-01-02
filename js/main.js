62f6b5ec54e7a265f361634b67faff77bdc9f568
KoTamaki <koukoukokoko@icloud.com>
2020-01-02 17:19:58 +0900

    make function fall()  renew main.js and style.css

--- js/main.js
+++ js/main.js
@@ -1,164 +1,31 @@
 "use strict";
 
-var array = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
-var move = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
+var field_W = 300,
+    field_H = 600;
+var cols = 10,
+    rows = 20;
+var block_W = field_W / cols,
+    block_H = field_H / rows;
+var canvas = document.getElementById('field');
+var ctx = canvas.getContext('2d');
+var current_x = 0,
+    current_y = 0;
+ctx.strokeStyle = "black";
+ctx.strokeRect(0, 0, block_W, block_H);
 
-var draw = function draw() {
-  $('#game').find('tr').each(function (i, elemTr) {
-    // trタグそれぞれに対する処理
-    $(elemTr).children().each(function (j, elemTd) {
-      // tdタグそれぞれに対する処理
-      $(elemTd).removeClass(); // まずはクラスをすべてなしにする
+var render = function render() {
+  ctx.clearRect(0, 0, field_W, field_H);
+  ctx.strokeStyle = "black";
 
-      switch (array[i][j]) {
-        case 1:
-          $(elemTd).addClass("stick"); // 1の時にはstickクラスを割り振る
-
-          break;
-
-        default:
-          $(elemTd).addClass("default");
-        // それ以外の時にはdefaultクラスを割り振る
-      }
-    });
-  });
-};
-
-var fall = function fall() {
-  var under = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]; // 下の行にブロックがあるかどうかを保持する配列
-
-  for (var i = 19; i >= 0; i--) {
-    for (var j = 0; j < 10; j++) {
-      if (under[j] == 0) {
-        if (array[i][j] == 0) {
-          // 下に何もなくそのマスがブロックでもないとき
-          under[j] = 0;
-        } else {
-          // 下に何もなくそのマスがブロックであるとき
-          array[i + 1][j] = array[i][j];
-          array[i][j] = 0;
-
-          if (move[i][j] == 1) {
-            move[i][j] = 0;
-            move[i + 1][j] = 1;
-          }
-
-          under[j] = 0;
-        }
-      } else {
-        if (array[i][j] == 0) {
-          // 下がブロックでそのマスがブロックでないとき
-          under[j] = 0;
-        } else {
-          if (move[i][j] == 1) {
-            resetMove();
-          } // 下がブロックでそのマスがブロックのとき
-
-
-          under[j] = 1;
-        }
-      }
-    }
+  for (var i = 0; i < cols; i++) {
+    ctx.strokeRect(i * block_W, current_y * block_H, block_W - 1, block_H - 1);
   }
 };
 
-var moveBlockRight = function moveBlockRight() {
-  for (var i = 19; i >= 0; i--) {
-    var newMove = move[i].concat();
-
-    for (var j = 8; j >= 0; j--) {
-      if (move[i][j] == 1) {
-        array[i][j + 1] = array[i][j];
-        array[i][j] = 0;
-        newMove[j + 1] = 1;
-        newMove[j] = 0;
-      }
-    }
-
-    move[i] = newMove;
-  }
+var tick = function tick() {
+  current_y++;
+  render();
 };
 
-var moveBlockLeft = function moveBlockLeft() {
-  for (var i = 19; i >= 0; i--) {
-    var newMove = move[i].concat();
-
-    for (var j = 1; j < 10; j++) {
-      if (move[i][j] == 1) {
-        array[i][j - 1] = array[i][j];
-        array[i][j] = 0;
-        newMove[j - 1] = 1;
-        newMove[j] = 0;
-      }
-    }
-
-    move[i] = newMove;
-  }
-};
-
-document.onkeydown = function (e) {
-  switch (e.code) {
-    case "Space":
-      tetBlock(1);
-      break;
-
-    case "ArrowRight":
-      moveBlockRight();
-      draw();
-      break;
-
-    case "ArrowLeft":
-      moveBlockLeft();
-      draw();
-      break;
-  }
-};
-
-var moveFlag = 0; // 動かす対象があるかどうか(0はない、1はあることを示す)
-
-var tetBlock = function tetBlock(blockNum) {
-  if (moveFlag == 0) {
-    switch (blockNum) {
-      case 1:
-        array[0][5] = blockNum;
-        array[1][5] = blockNum;
-        array[2][5] = blockNum;
-        array[3][5] = blockNum;
-        move[0][5] = 1;
-        move[1][5] = 1;
-        move[2][5] = 1;
-        move[3][5] = 1;
-        break;
-    }
-
-    moveFlag = 1;
-  }
-};
-
-var resetMove = function resetMove() {
-  moveFlag = 0;
-
-  for (var i = 0; i < 20; i++) {
-    for (var j = 0; j < 10; j++) {
-      move[i][j] = 0;
-    }
-  }
-};
-
-var checkDelete = function checkDelete() {
-  for (var i = 19; i >= 0; i--) {
-    if (!array[i].includes(0)) {
-      for (var j = 0; j < 10; j++) {
-        array[i][j] = 0;
-      }
-    }
-  }
-};
-
-draw(); // 読込が完了したらまず表示
-
-setInterval(function () {
-  checkDelete();
-  fall();
-  draw();
-}, 500); // 0.5秒ごとに表示を更新していきます
+render();
+setInterval(tick, 500);
